b1ea14e91da6796563d72cdd2e4357652d29132e
Marc Durepos <marc@bemade.org>
2024-03-22 08:03:50 -0400

    add an empty mass_editing module for upgrade

---
+++ mass_editing/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo Module Developed by Candidroot Solutions Pvt. Ltd.
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Empty Module',
+    'category': 'Discuss',
+    'summary': 'Empty module',
+    'version': '17.0.0.0',
+    'author': "Candidroot Solutions Pvt. Ltd.",
+    'depends': [],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
